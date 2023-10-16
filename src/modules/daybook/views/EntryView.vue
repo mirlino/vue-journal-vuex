a0df6654ee30ed2485c7b9904b5daace6d518f57
Mirlino_Marine <mirlinomarine@gmail.com>
2023-10-16 11:45:41 -0400

    Adding new features

--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -1,42 +1,48 @@
 <template>
-  <div class="entry-title d-flex justify-content-between p-2">
-    <div>
-        <span class="text-success fs-3 fw-bold">15</span>
-        <span class="mx-1 fs-3">Julio</span>
-        <span class="mx-2 fs-4 fw-light">2023, Jueves</span>
+  <template v-if="entry">
+    <div class="entry-title d-flex justify-content-between p-2">
+      <div>
+          <span class="text-success fs-3 fw-bold">{{ day }}</span>
+          <span class="mx-1 fs-3">{{ month }}</span>
+          <span class="mx-2 fs-4 fw-light">{{ yearDay }}</span>
+      </div>
+
+      <div>
+        <button class="btn btn-danger mx-2">
+          Borrar
+          <i class="fa fa-trash-alt"></i>
+        </button>
+        <button class="btn btn-primary mx-2">
+          Subir foto
+          <i class="fa fa-upload"></i>
+        </button>
+      </div>
     </div>
 
-    <div>
-      <button class="btn btn-danger mx-2">
-        Borrar
-        <i class="fa fa-trash-alt"></i>
-      </button>
-      <button class="btn btn-primary mx-2">
-        Subir foto
-        <i class="fa fa-upload"></i>
-      </button>
+    <hr>
+    <div class="d-flex flex-column px-3 h-75">
+      <textarea 
+        v-model="entry.text"
+        placeholder="¿Qué sucedió hoy?"
+      ></textarea>
     </div>
-  </div>
+    
+    <img src="https://photoshop-kopona.com/uploads/posts/2018-09/1538126513_3.jpg" 
+      alt="entry-picture"
+      class="img-thumbnail"
+    />
+  </template>
 
-  <hr>
-  <div class="d-flex flex-column px-3 h-75">
-    <textarea 
-      placeholder="¿Qué sucedió hoy?"
-    ></textarea>
-  </div>
   <the-fab
     icon="fa-save"  
   ></the-fab>
 
-  <img src="https://photoshop-kopona.com/uploads/posts/2018-09/1538126513_3.jpg" 
-    alt="entry-picture"
-    class="img-thumbnail"
-  />
 </template>
 
 <script>
 import { defineAsyncComponent } from 'vue';
 import { mapGetters } from 'vuex';
+import getDayMonthYear from '../helpers/getDayMonthYear'
 
   export default {
     props: {
@@ -45,23 +51,49 @@ import { mapGetters } from 'vuex';
         required: true
       }
     },
+    data(){
+      return {
+        entry: null
+      }
+    },
 
     components: {
       TheFab: defineAsyncComponent( () => import('../components/TheFab.vue'))
+      
     },
 
     computed:{
       ...mapGetters('journal', ['getEntryById']),
+      day(){
+        const { day } = getDayMonthYear( this.entry.date )
+        return day
+      },
+      month(){
+        const { month } = getDayMonthYear( this.entry.date )
+        return month
+      },
+      yearDay(){
+        const { yearDay } = getDayMonthYear( this.entry.date )
+        return yearDay
+      }
     },
 
     methods: {
       loadEntry(){
         const entry = this.getEntryById( this.id )
-        console.log(entry)
+        if( !entry ) return this.$router.push( { name: 'no-entry' } )
+
+        this.entry = entry
       }
     },
     created() {
       this.loadEntry()
+    },
+
+    watch: {
+      id() {
+        this.loadEntry()
+      }
     }
 
   }
